9afca40f8d079f29cbf647fd331e2f167acf7f83
josh <josguther@mesoamericana.edu.gt>
2019-11-20 01:59:19 -0600

    avance frappe link 12

---
+++ apps/library_management/README.md
@@ -0,0 +1,7 @@
+## Gestion de Biblioteca
+
+Aplicación para gestionar artículos, miembros, membresías y transacciones para bibliotecas
+
+#### License
+
+Licencia pública general de GNU
---
+++ apps/library_management/library_management/config/desktop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Gestion de Biblioteca",
+			"color": "#7DCEA0",
+			"icon": "octicon octicon-book",
+			"type": "module",
+			"label": _("Gestion de Biblioteca")
+		}
+	]
